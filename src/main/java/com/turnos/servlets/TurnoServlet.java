a984b7de5155969f5bfb2e97bb904bf59f55a952
Fernando <feripa.6@gmail.com>
2025-12-01 21:26:10 +0100

    Servlet turno con errores aun

--- src/main/java/com/turnos/servlets/TurnoServlet.java
+++ src/main/java/com/turnos/servlets/TurnoServlet.java
@@ -1,4 +1,40 @@
 package com.turnos.servlets;
 
-public class TurnoServlet {
+import com.turnos.entities.Ciudadano;
+import com.turnos.entities.Turno;
+import com.turnos.entities.TurnoEstado;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/listaTurnos")
+public class TurnoServlet extends HttpServlet {
+
+    private List<Turno> turnos = new ArrayList<>();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("listarTurnos", turnos);
+        req.getRequestDispatcher("jsp/listar-ciudadanos.jsp")
+                .forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idProgresivo = req.getParameter("idProgresivo");
+        TurnoEstado estado = TurnoEstado.valueOf(req.getParameter("estado"));
+        LocalDateTime fecha = LocalDateTime.parse(req.getParameter("fecha"));
+        String descripcionTramite = req.getParameter("descripcionTramite");
+        String dniCiudadano = req.getParameter("dni");
+        Ciudadano ciudadano = req.getParameter(ciudadano);
+
+        Turno t = new Turno(idProgresivo, estado, fecha, descripcionTramite, ciudadano);
+        resp.sendRedirect("turno");
+    }
 }
